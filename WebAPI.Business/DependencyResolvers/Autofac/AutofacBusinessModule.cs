04add78615e46c52eb27785b1c8a45986e41f3e7
Furkan Aydin <furknn2112@gmail.com>
2022-03-11 13:58:23 +0300

    CREATE (LAYER:Business) : DependencyResolvers  konfigürasyonu yapıldı.

---
+++ WebAPI.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Business.Abstact;
+using WebAPI.Business.Concrete;
+using WebAPI.DataAccess.Abstract;
+using WebAPI.DataAccess.Concrete.EntityFramework;
+
+namespace WebAPI.Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductRepository>().As<IProductRepository>().SingleInstance();
+        }
+    }
+}
+
