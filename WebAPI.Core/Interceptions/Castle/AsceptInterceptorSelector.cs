74a36d88dc58af142a0fb963dfffa0f6ab90f06c
Furkan Aydin <furknn2112@gmail.com>
2022-03-11 22:40:01 +0300

    CREATE (Layer:Core) : AsceptInterceptorSelector

---
+++ WebAPI.Core/Interceptions/Castle/AsceptInterceptorSelector.cs
@@ -0,0 +1,24 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Core.Interceptions.Castle
+{
+    public class AsceptInterceptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAttribute = type.GetCustomAttributes<MethodInterceptionBaseAttribute>(true).ToList();
+
+            var methodAttribute = type.GetMethod(method.Name)?.GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+
+            if (!methodAttribute.Equals(null))
+                classAttribute.AddRange(methodAttribute);
+            return classAttribute.OrderBy(a => a.Priority).ToArray();
+        }
+    }
+}
