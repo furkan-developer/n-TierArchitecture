ecec905871eff2950e94ba7132d73148fd5f9884
Furkan Aydin <furknn2112@gmail.com>
2022-03-11 22:18:27 +0300

    CREATE (Layer:Core) : MethodInterception

---
+++ WebAPI.Core/Interceptions/Castle/MethodInterception.cs
@@ -0,0 +1,39 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Core.Interceptions.Castle
+{
+    public class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            bool successflag = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception ex)
+            {
+                successflag = false;
+                OnException(invocation,ex);
+                throw;
+            }
+            finally
+            {
+                if (successflag)
+                    OnSuccess(invocation);
+            }
+            OnAfter(invocation);
+        }
+
+        public virtual void OnBefore(IInvocation ınvocation) { }
+        public virtual void OnAfter(IInvocation ınvocation) { }
+        public virtual void OnSuccess(IInvocation ınvocation) { }
+        public virtual void OnException(IInvocation ınvocation,Exception ex) { }
+    }
+}
