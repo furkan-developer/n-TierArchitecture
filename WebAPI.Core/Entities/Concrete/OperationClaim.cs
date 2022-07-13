0f40df18b8e13c4739f1f9294e022689297fa74e
Furkan Aydin <furknn2112@gmail.com>
2022-07-13 19:14:32 +0300

    JWT veritabanı nesneleri ve appsettings.json yapılandırma formatı eklendi

---
+++ WebAPI.Core/Entities/Concrete/OperationClaim.cs
@@ -0,0 +1,10 @@
+﻿using WebAPI.Core.Entities.Abstract;
+
+namespace WebAPI.Core.Entities.Concrete
+{
+    public class OperationClaim : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
