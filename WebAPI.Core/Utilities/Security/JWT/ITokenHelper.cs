4cb59bcab3ef29de978fa820f1e333a4ef392672
Furkan Aydin <furknn2112@gmail.com>
2022-07-14 12:09:28 +0300

    Token oluşturacak tool için altyapı tasarlandı

---
+++ WebAPI.Core/Utilities/Security/JWT/ITokenHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using WebAPI.Core.Entities.Concrete;
+
+namespace WebAPI.Core.Utilities.Security.JWT
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
+    }
+}
