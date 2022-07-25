0c46331704fb58025ad89171c21732f247b80723
Furkan Aydin <furknn2112@gmail.com>
2022-07-25 16:33:42 +0300

    Cache' den data silme işlemi attribute seviyesinde sisteme dahil edildi.

---
+++ WebAPI.Core/Ascepts/Autofac/Caching/CacheRemoveAspect.cs
@@ -0,0 +1,30 @@
+﻿using Castle.DynamicProxy;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Core.CrossCuttingConcerns.Caching.Abstract;
+using WebAPI.Core.Interceptions.Castle;
+using WebAPI.Core.IoC;
+
+namespace WebAPI.Core.Ascepts.Autofac.Caching
+{
+    public class CacheRemoveAspect : MethodInterception
+    {
+        private string _pattern;
+        private ICacheManager _cacheManager;
+
+        public CacheRemoveAspect(string pattern)
+        {
+            _pattern = pattern;
+            _cacheManager = ServiceTool.ServiceProvider.GetService<ICacheManager>();
+        }
+
+        protected override void OnSuccess(IInvocation invocation)
+        {
+            _cacheManager.RemoveByPattern(_pattern);
+        }
+    }
+}
