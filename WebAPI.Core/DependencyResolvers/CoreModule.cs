33502336ca0da5cec7ab30559c6ed80589bc8be2
Furkan Aydin <furknn2112@gmail.com>
2022-07-25 14:24:39 +0300

    Sisteme Cache altyapısı eklendi.

--- WebAPI.Core/DependencyResolvers/CoreModule.cs
+++ WebAPI.Core/DependencyResolvers/CoreModule.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebAPI.Core.CrossCuttingConcerns.Caching.Abstract;
+using WebAPI.Core.CrossCuttingConcerns.Caching.Concrete.Microsoft;
 using WebAPI.Core.IoC;
 
 namespace WebAPI.Core.DependencyResolvers
@@ -12,8 +14,11 @@ namespace WebAPI.Core.DependencyResolvers
     public class CoreModule : ICoreModule
     {
         public void Load(IServiceCollection services)
-        {
+        {         
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddSingleton<ICacheManager, MemoryCacheManager>();
+            services.AddMemoryCache();
         }
     }
 }
