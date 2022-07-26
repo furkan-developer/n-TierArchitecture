d3734c4a3191aeb26bd8208f2b7bf7aad3a187ad
Furkan Aydin <furknn2112@gmail.com>
2022-07-26 07:54:22 +0300

    Transaction Ascept sisteme dahil edildi.

---
+++ WebAPI.Core/Ascepts/Autofac/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,31 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using WebAPI.Core.Interceptions.Castle;
+
+namespace WebAPI.Core.Ascepts.Autofac.Transection
+{
+    public class TransactionScopeAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope transactionScope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Complete();
+                }
+                catch (System.Exception ex)
+                {
+                    transactionScope.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
